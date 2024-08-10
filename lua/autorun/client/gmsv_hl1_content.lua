565929704f031e3e2f13e7ce30711c18d64c73bd
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2024-08-09 20:52:24 -0400

    Gonna try something else
    Too janky

--- lua/autorun/client/gmsv_hl1_content.lua
+++
@@ -1,7 +0,0 @@
-if not IsMounted("hl1mp") then
-	steamworks.DownloadUGC("338269021", function(Path)
-		if not isstring(Path) or string.len(Path) < 1 then return end
-
-		game.MountGMA(Path)
-	end)
-end
